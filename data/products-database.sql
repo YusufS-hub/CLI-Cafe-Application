81faa266613016718767d492bc3430b1f844da31
Yusuf Salad <147105822+KillerQueen-1@users.noreply.github.com>
2025-09-06 21:42:02 +0100

    Completed Mini Project

--- data/products-database.sql
+++ data/products-database.sql
@@ -1,16 +1,8 @@
-/*
-Used in the Python part of the session.
-Sample full set of SQL for the person table.
-This file can be run in an Adminer window vs MySQL.
-The `my_db_app_solution.py` file assumes this is the SQL that has been run in Adminer.
-...and that the `databases/handouts/docker-compose.yml` matches what is running in docker.
-*/
+
 
 DROP DATABASE IF EXISTS mini;
 
 CREATE DATABASE mini;
-
--- Select test database in the DB dropdown
 
 CREATE TABLE products (
   product_id INTEGER GENERATED ALWAYS AS IDENTITY,
